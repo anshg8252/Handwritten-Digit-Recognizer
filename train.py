d0c3bc7778e6162a00abe4666eee05068291186e
anshg8252 <anshg6910@gmail.com>
2026-08-10 18:28:58 +0530

    Preprocess MNIST images

--- train.py
+++ train.py
@@ -4,6 +4,12 @@ from tensorflow.keras import datasets
 # Load MNIST dataset
 (x_train, y_train), (x_test, y_test) = datasets.mnist.load_data()
 
+print("Training images:", x_train.shape)
+print("Training labels:", y_train.shape)
+print("Testing images:", x_test.shape)
+print("Testing labels:", y_test.shape)
+
+
 # Normalize pixel values from 0-255 to 0-1
 x_train = x_train / 255.0
 x_test = x_test / 255.0
